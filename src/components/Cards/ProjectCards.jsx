2bc5cb7f0f607abb420f4a9146d343fcd7796176
Hachimankira <kiran9860819025@gmail.com>
2024-08-31 19:51:05 +0545

    final

--- src/components/Cards/ProjectCards.jsx
+++ src/components/Cards/ProjectCards.jsx
@@ -130,7 +130,7 @@ const ProjectCards = ({project,setOpenModal}) => {
             <Image src={project.image[0]}/>
             <Tags>
                 {project.tags?.map((tag, index) => (
-                <Tag>{tag}</Tag>
+                <Tag key={index}>{tag}</Tag>
                 ))}
             </Tags>
             <Details>
@@ -139,8 +139,8 @@ const ProjectCards = ({project,setOpenModal}) => {
                 <Description>{project.description}</Description>
             </Details>
             <Members>
-                {project.member?.map((member) => (
-                    <Avatar src={member.img}/>
+                {project.member?.map((member, index) => (
+                    <Avatar key={index} src={member.img}/>
                 ))}
             </Members>
             {/* <Button>View Project</Button> */}
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -129,15 +129,15 @@ const Skills = () => {
     <Container id="skills">
       <Wrapper>
         <Title>Skills</Title>
-        <Desc>Here are some of my skills on which I have been working on for the past 1 years.
+        <Desc>Here are some of my skills on which I have been working on for the past 2 years.
         </Desc>
         <SkillsContainer>
-          {skills.map((skill) => (
-            <Skill>
+          {skills.map((skill, index) => (
+            <Skill key={index}>
               <SkillTitle>{skill.title}</SkillTitle>
               <SkillList>
-                {skill.skills.map((item) => (
-                  <SkillItem>
+                {skill.skills.map((item, index) => (
+                  <SkillItem key={index}>
                     <SkillImage src={item.image} />
                     {/* {item.image} */}
                     {item.name}
